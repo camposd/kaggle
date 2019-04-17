d25c2cb2b01da59a4cd573a8ef68e807dbdfab1c
Diego Campos <diegocrubi@gmail.com>
2019-04-17 06:31:45 -0500

    prepping

--- R/01_zillow.R
+++ R/01_zillow.R
@@ -13,3 +13,23 @@ library(ggrepel)
 library(randomForest)
 library(psych)
 library(xgboost)
+
+
+# Reading in Data ---------------------------------------------------------
+
+train <- read.csv("data/train.csv", stringsAsFactors = FALSE)
+test <- read.csv("data/test.csv",stringsAsFactors = FALSE)
+
+
+# Data and Size Structure -------------------------------------------------
+dim(train)
+str(train[,c(1:10,81)])
+
+test_labels <- test$Id
+test$Id <- NULL
+train$Id <- NULL
+
+test$SalePrice <- NA
+
+all <- rbind(train,test)
+dim(all)
